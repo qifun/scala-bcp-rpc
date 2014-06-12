7aa5ebeef068b2c07c6beb68408d4bab98bc1f1f
杨博 <pop.atry@gmail.com>
2014-06-12 15:52:25 +0800

    增加隐式转换

---
+++ build.sbt
@@ -0,0 +1,3 @@
+libraryDependencies += "com.qifun" %% "stateless-future" % "0.2.2"
+
+libraryDependencies += "com.qifun" %% "stateless-future-util" % "0.4.0-SNAPSHOT"
---
+++ src/main/scala/com/qifun/hddl/asyncProxy/Implicits.scala
@@ -0,0 +1,18 @@
+package com.qifun.hddl.asyncProxy
+
+import com.qifun.statelessFuture._
+import scala.runtime.BoxedUnit
+
+object Implicits {
+
+  /**
+   * 把`future`转换成其运行时类型。
+   * 
+   * 由于Haxe生成的Java代码不支持Scala的[[Unit]]类型，所以只能使用运行时类型。
+   * 当在Scala中实现Haxe生成的接口时，如果涉及上述运行时类型，就必须进行本转换，编译器才会放行。
+   */
+  implicit def boxedFuture[AwaitResult](future: Future[AwaitResult]): Awaitable[AwaitResult, BoxedUnit] = {
+    future.asInstanceOf[Awaitable[AwaitResult, BoxedUnit]]
+  }
+
+}
